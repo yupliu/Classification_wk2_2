bc49f55aa8402dbd97887eec96f6d3b6dcaf0075
Alan Yu <yupliu@users.noreply.github.com>
2017-08-11 15:56:24 -0400

    Almost

--- Classification_wk2_2/Classification_wk2_2/Classification_wk2_2.py
+++ Classification_wk2_2/Classification_wk2_2/Classification_wk2_2.py
@@ -10,12 +10,15 @@ try:
 except ImportError:
     pass
 
-products = graphlab.SFrame('C:\\Machine_Learning\\Classification_wk2\\amazon_baby_subset.gl\\')
+#products = graphlab.SFrame('C:\\Machine_Learning\\Classification_wk2\\amazon_baby_subset.gl\\')
+products = graphlab.SFrame('D:\\ML_Learning\\UW_Classification\\week2\\amazon_baby_subset.gl\\')
+
 print '# of positive reviews =', len(products[products['sentiment']==1])
 print '# of negative reviews =', len(products[products['sentiment']==-1])
 
 import json
-with open('C:\\Machine_Learning\\Classification_wk2\\important_words.json','r') as f:
+#with open('C:\\Machine_Learning\\Classification_wk2\\important_words.json','r') as f:
+with open('D:\\ML_Learning\\UW_Classification\\week2\\important_words.json','r') as f:
     important_words = json.load(f)
 important_words = [str(s) for s in important_words]
 print important_words
@@ -26,9 +29,10 @@ def remove_punctuation(text):
 products['review_clean'] = products['review'].apply(remove_punctuation)
 for word in important_words:
     products[word] = products['review_clean'].apply(lambda s:s.split().count(word))
-products['perfect']
-products['contains_perfect'] = products['perfect'].apply(lambda x: 1 if x >= 1 else 0)
-print products['contains_perfect'].sum()
+
+train_data,validation_data = products.random_split(.8,seed=2)
+print 'Training set   : %d data points' % len(train_data)
+print 'Validation set : %d data points' % len(validation_data)
 
 def get_numpy_data(data_sframe, features, label):
     data_sframe['intercept'] = 1
@@ -40,9 +44,8 @@ def get_numpy_data(data_sframe, features, label):
     return(feature_matrix, label_array)
 
 # Warning: This may take a few minutes...
-feature_matrix, sentiment = get_numpy_data(products, important_words, 'sentiment') 
-feature_matrix.shape
-len(important_words)
+feature_matrix_train, sentiment_train = get_numpy_data(train_data, important_words, 'sentiment')
+feature_matrix_valid, sentiment_valid = get_numpy_data(validation_data, important_words, 'sentiment')
 
 '''
 produces probablistic estimate for P(y_i = +1 | x_i, w).
@@ -59,59 +62,29 @@ def predict_probability(feature_matrix, coefficients):
     # return predictions
     return predictions
 
-dummy_feature_matrix = np.array([[1.,2.,3.], [1.,-1.,-1]])
-dummy_coefficients = np.array([1., 3., -1.])
-
-correct_scores      = np.array( [ 1.*1. + 2.*3. + 3.*(-1.),          1.*1. + (-1.)*3. + (-1.)*(-1.) ] )
-correct_predictions = np.array( [ 1./(1+np.exp(-correct_scores[0])), 1./(1+np.exp(-correct_scores[1])) ] )
-
-print 'The following outputs must match '
-print '------------------------------------------------'
-print 'correct_predictions           =', correct_predictions
-print 'output of predict_probability =', predict_probability(dummy_feature_matrix,dummy_coefficients)
-
-def feature_derivative(errors, feature):     
+def feature_derivative_with_L2(errors, feature,coefficent,l2_penalty,feature_is_constant):     
     # Compute the dot product of errors and feature
-    derivative = np.dot(errors,feature)    
+    derivative = np.dot(errors,feature)
+    if not feature_is_constant:
+        derivative = derivative - 2*l2_penalty*coefficent    
     # Return the derivative
     return derivative
 
-def compute_log_likelihood(feature_matrix, sentiment, coefficients):
+def compute_log_likelihood_with_L2(feature_matrix, sentiment, coefficients, l2_penalty):
     indicator = (sentiment==+1)
     scores = np.dot(feature_matrix, coefficients)
-    logexp = np.log(1. + np.exp(-scores))
     
-    # Simple check to prevent overflow
-    mask = np.isinf(logexp)
-    logexp[mask] = -scores[mask]
+    lp = np.sum((indicator-1)*scores - np.log(1. + np.exp(-scores))) - l2_penalty*np.sum(coefficients[1:]**2)
     
-    lp = np.sum((indicator-1)*scores - logexp)
     return lp
-
-dummy_feature_matrix = np.array([[1.,2.,3.], [1.,-1.,-1]])
-dummy_coefficients = np.array([1., 3., -1.])
-dummy_sentiment = np.array([-1, 1])
-
-correct_indicators  = np.array( [ -1==+1,                                       1==+1 ] )
-correct_scores      = np.array( [ 1.*1. + 2.*3. + 3.*(-1.),                     1.*1. + (-1.)*3. + (-1.)*(-1.) ] )
-correct_first_term  = np.array( [ (correct_indicators[0]-1)*correct_scores[0],  (correct_indicators[1]-1)*correct_scores[1] ] )
-correct_second_term = np.array( [ np.log(1. + np.exp(-correct_scores[0])),      np.log(1. + np.exp(-correct_scores[1])) ] )
-
-correct_ll          =      sum( [ correct_first_term[0]-correct_second_term[0], correct_first_term[1]-correct_second_term[1] ] ) 
-
-print 'The following outputs must match '
-print '------------------------------------------------'
-print 'correct_log_likelihood           =', correct_ll
-print 'output of compute_log_likelihood =', compute_log_likelihood(dummy_feature_matrix, dummy_sentiment, dummy_coefficients)
 
 from math import sqrt
 
-def logistic_regression(feature_matrix, sentiment, initial_coefficients, step_size, max_iter):
+def logistic_regression_with_L2(feature_matrix, sentiment, initial_coefficients, step_size, l2_penalty, max_iter):
     coefficients = np.array(initial_coefficients) # make sure it's a numpy array
     for itr in xrange(max_iter):
-
         # Predict P(y_i = +1|x_i,w) using your predict_probability() function
-        # YOUR CODE HERE
+        ## YOUR CODE HERE
         predictions = predict_probability(feature_matrix,coefficients)
         
         # Compute indicator value for (y_i = +1)
@@ -120,53 +93,145 @@ def logistic_regression(feature_matrix, sentiment, initial_coefficients, step_si
         # Compute the errors as indicator - predictions
         errors = indicator - predictions
         for j in xrange(len(coefficients)): # loop over each coefficient
-            
+            is_intercept = (j == 0)
             # Recall that feature_matrix[:,j] is the feature column associated with coefficients[j].
             # Compute the derivative for coefficients[j]. Save it in a variable called derivative
-            # YOUR CODE HERE
-            derivative = feature_derivative(errors,feature_matrix[:,j])
+            ## YOUR CODE HERE
+            derivative = feature_derivative_with_L2(errors,feature_matrix[:,j],coefficients[j],l2_penalty,is_intercept)
             
             # add the step size times the derivative to the current coefficient
             ## YOUR CODE HERE
-            coefficients[j] = coefficients[j] + step_size * derivative        
+            coefficients[j] = coefficients[j] + step_size * derivative
+        
         # Checking whether log likelihood is increasing
-        if itr <= 15 or (itr <= 100 and itr % 10 == 0) or (itr <= 1000 and itr % 100 == 0) or (itr <= 10000 and itr % 1000 == 0) or itr % 10000 == 0:
-            lp = compute_log_likelihood(feature_matrix, sentiment, coefficients)
-            print 'iteration %*d: log likelihood of observed labels = %.8f' %(int(np.ceil(np.log10(max_iter))), itr, lp)
+        if itr <= 15 or (itr <= 100 and itr % 10 == 0) or (itr <= 1000 and itr % 100 == 0) \
+        or (itr <= 10000 and itr % 1000 == 0) or itr % 10000 == 0:
+            lp = compute_log_likelihood_with_L2(feature_matrix, sentiment, coefficients, l2_penalty)
+            print 'iteration %*d: log likelihood of observed labels = %.8f' % \
+                (int(np.ceil(np.log10(max_iter))), itr, lp)
     return coefficients
 
-coefficients = logistic_regression(feature_matrix, sentiment, initial_coefficients=np.zeros(194),
-                                   step_size=1e-7, max_iter=301)
-print coefficients
+# run with L2 = 0
+coefficients_0_penalty = logistic_regression_with_L2(feature_matrix_train, sentiment_train,
+                                                     initial_coefficients=np.zeros(194),
+                                                     step_size=5e-6, l2_penalty=0, max_iter=501)
 
-# Compute the scores as a dot product between feature_matrix and coefficients.
-scores = np.dot(feature_matrix, coefficients)
-print scores
+# run with L2 = 4
+coefficients_4_penalty = logistic_regression_with_L2(feature_matrix_train, sentiment_train,
+                                                      initial_coefficients=np.zeros(194),
+                                                      step_size=5e-6, l2_penalty=4, max_iter=501)
+# run with L2 = 10
+coefficients_10_penalty = logistic_regression_with_L2(feature_matrix_train, sentiment_train,
+                                                      initial_coefficients=np.zeros(194),
+                                                      step_size=5e-6, l2_penalty=10, max_iter=501)
+# run with L2 = 1e2
+coefficients_1e2_penalty = logistic_regression_with_L2(feature_matrix_train, sentiment_train,
+                                                       initial_coefficients=np.zeros(194),
+                                                       step_size=5e-6, l2_penalty=1e2, max_iter=501)
+# run with L2 = 1e3
+coefficients_1e3_penalty = logistic_regression_with_L2(feature_matrix_train, sentiment_train,
+                                                       initial_coefficients=np.zeros(194),
+                                                       step_size=5e-6, l2_penalty=1e3, max_iter=501)
+# run with L2 = 1e5
+coefficients_1e5_penalty = logistic_regression_with_L2(feature_matrix_train, sentiment_train,
+                                                       initial_coefficients=np.zeros(194),
+                                                       step_size=5e-6, l2_penalty=1e5, max_iter=501)
+table = graphlab.SFrame({'word': ['(intercept)'] + important_words})
+def add_coefficients_to_table(coefficients, column_name):
+    table[column_name] = coefficients
+    return table
+add_coefficients_to_table(coefficients_0_penalty, 'coefficients [L2=0]')
+add_coefficients_to_table(coefficients_4_penalty, 'coefficients [L2=4]')
+add_coefficients_to_table(coefficients_10_penalty, 'coefficients [L2=10]')
+add_coefficients_to_table(coefficients_1e2_penalty, 'coefficients [L2=1e2]')
+add_coefficients_to_table(coefficients_1e3_penalty, 'coefficients [L2=1e3]')
+add_coefficients_to_table(coefficients_1e5_penalty, 'coefficients [L2=1e5]')
 
-prob = predict_probability(feature_matrix,coefficients)
-print prob
-postive_review = prob[prob>0.5]
-len(postive_review)
-
-conv = lambda x: +1 if x > 0.5 else -1
-pred = np.vectorize(conv)
-pred = pred(prob)
-diff = pred - sentiment
-correct = diff[diff==0]
-correct = len(correct)
-total = len(sentiment)
-
-num_mistakes = total-correct # YOUR CODE HERE
-accuracy = correct / total # YOUR CODE HERE
-print "-----------------------------------------------------"
-print '# Reviews   correctly classified =', len(products) - num_mistakes
-print '# Reviews incorrectly classified =', num_mistakes
-print '# Reviews total                  =', len(products)
-print "-----------------------------------------------------"
-print 'Accuracy = %.2f' % accuracy
-
-coefficients = list(coefficients[1:]) # exclude intercept
-word_coefficient_tuples = [(word, coefficient) for word, coefficient in zip(important_words, coefficients)]
+coefficients_0_penalty = list(coefficients_0_penalty[1:]) # exclude intercept
+word_coefficient_tuples = [(word, coefficients_0_penalty) for word, coefficients_0_penalty in zip(important_words, coefficients_0_penalty)]
 word_coefficient_tuples = sorted(word_coefficient_tuples, key=lambda x:x[1], reverse=True)
 
+positive_words = ['love','loves','easy','perfect','great']
+negative_words = ['returned','waste','return','money','disappointed']
+
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = 10, 6
+
+def make_coefficient_plot(table, positive_words, negative_words, l2_penalty_list):
+    cmap_positive = plt.get_cmap('Reds')
+    cmap_negative = plt.get_cmap('Blues')
+    
+    xx = l2_penalty_list
+    plt.plot(xx, [0.]*len(xx), '--', lw=1, color='k')
+    
+    table_positive_words = table.filter_by(column_name='word', values=positive_words)
+    table_negative_words = table.filter_by(column_name='word', values=negative_words)
+    del table_positive_words['word']
+    del table_negative_words['word']
+    
+    for i in xrange(len(positive_words)):
+        color = cmap_positive(0.8*((i+1)/(len(positive_words)*1.2)+0.15))
+        plt.plot(xx, table_positive_words[i:i+1].to_numpy().flatten(),
+                 '-', label=positive_words[i], linewidth=4.0, color=color)
+        
+    for i in xrange(len(negative_words)):
+        color = cmap_negative(0.8*((i+1)/(len(negative_words)*1.2)+0.15))
+        plt.plot(xx, table_negative_words[i:i+1].to_numpy().flatten(),
+                 '-', label=negative_words[i], linewidth=4.0, color=color)
+        
+    plt.legend(loc='best', ncol=3, prop={'size':16}, columnspacing=0.5)
+    plt.axis([1, 1e5, -1, 2])
+    plt.title('Coefficient path')
+    plt.xlabel('L2 penalty ($\lambda$)')
+    plt.ylabel('Coefficient value')
+    plt.xscale('log')
+    plt.rcParams.update({'font.size': 18})
+    plt.tight_layout()
+
+make_coefficient_plot(table, positive_words, negative_words, l2_penalty_list=[0, 4, 10, 1e2, 1e3, 1e5])
+
+def get_classification_accuracy(feature_matrix, sentiment, coefficients):
+    scores = np.dot(feature_matrix, coefficients)
+    apply_threshold = np.vectorize(lambda x: 1. if x > 0  else -1.)
+    predictions = apply_threshold(scores)
+    
+    num_correct = (predictions == sentiment).sum()
+    accuracy = num_correct / len(feature_matrix)    
+    return accuracy
+
+train_accuracy = {}
+train_accuracy[0]   = get_classification_accuracy(feature_matrix_train, sentiment_train, coefficients_0_penalty)
+train_accuracy[4]   = get_classification_accuracy(feature_matrix_train, sentiment_train, coefficients_4_penalty)
+train_accuracy[10]  = get_classification_accuracy(feature_matrix_train, sentiment_train, coefficients_10_penalty)
+train_accuracy[1e2] = get_classification_accuracy(feature_matrix_train, sentiment_train, coefficients_1e2_penalty)
+train_accuracy[1e3] = get_classification_accuracy(feature_matrix_train, sentiment_train, coefficients_1e3_penalty)
+train_accuracy[1e5] = get_classification_accuracy(feature_matrix_train, sentiment_train, coefficients_1e5_penalty)
+
+validation_accuracy = {}
+validation_accuracy[0]   = get_classification_accuracy(feature_matrix_valid, sentiment_valid, coefficients_0_penalty)
+validation_accuracy[4]   = get_classification_accuracy(feature_matrix_valid, sentiment_valid, coefficients_4_penalty)
+validation_accuracy[10]  = get_classification_accuracy(feature_matrix_valid, sentiment_valid, coefficients_10_penalty)
+validation_accuracy[1e2] = get_classification_accuracy(feature_matrix_valid, sentiment_valid, coefficients_1e2_penalty)
+validation_accuracy[1e3] = get_classification_accuracy(feature_matrix_valid, sentiment_valid, coefficients_1e3_penalty)
+validation_accuracy[1e5] = get_classification_accuracy(feature_matrix_valid, sentiment_valid, coefficients_1e5_penalty)
+
+# Build a simple report
+for key in sorted(validation_accuracy.keys()):
+    print "L2 penalty = %g" % key
+    print "train accuracy = %s, validation_accuracy = %s" % (train_accuracy[key], validation_accuracy[key])
+    print "--------------------------------------------------------------------------------"
+
+# Optional. Plot accuracy on training and validation sets over choice of L2 penalty.
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = 10, 6
+
+sorted_list = sorted(train_accuracy.items(), key=lambda x:x[0])
+plt.plot([p[0] for p in sorted_list], [p[1] for p in sorted_list], 'bo-', linewidth=4, label='Training accuracy')
+sorted_list = sorted(validation_accuracy.items(), key=lambda x:x[0])
+plt.plot([p[0] for p in sorted_list], [p[1] for p in sorted_list], 'ro-', linewidth=4, label='Validation accuracy')
+plt.xscale('symlog')
+plt.axis([0, 1e3, 0.78, 0.786])
+plt.legend(loc='lower left')
+plt.rcParams.update({'font.size': 18})
+plt.tight_layout
 
